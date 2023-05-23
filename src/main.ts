6a4424e9ec3dd4f5f9ed03b2e33dae067af8d3fb
Kristijan <kikaccc29@gmail.com>
2023-05-23 16:58:17 +0200

    'first-commit'

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {AppRoutingModule} from "./app/app-routing.module";
+import {importProvidersFrom} from "@angular/core";
+// import {AnalyticsService} from "./app/shared/analytics.service";
+
+
+
+
+bootstrapApplication(AppComponent,{
+  providers:[
+    // AnalyticsService,
+    importProvidersFrom(AppRoutingModule)
+  ]
+})
